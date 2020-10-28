ec0d515180d5c83522c7ca424f63568fdeb1dd9a
holden <holdensheftel@gmail.com>
2020-10-28 13:51:21 -0600

    Modified close button symbol

--- src/TreeInterface/Node.jsx
+++ src/TreeInterface/Node.jsx
@@ -5,7 +5,7 @@ export const Node = ({ node, addChild, removeChild, editName }) => {
   return (
     <div className={`node${node.id} node`}>
       <button id="delete" onClick={() => removeChild(node)}>
-        X
+        &times;
       </button>
       {node.name}
       <button onClick={() => editName(node)}>edit</button>
