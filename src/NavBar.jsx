cefae06a9f37b152127095892c8ac6e67aae064c
holden <holdensheftel@gmail.com>
2020-10-28 21:24:43 -0600

    Improved nav bar styling and added modal

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavBar.css";
 
+const Modal = props => {
+  return (
+    <div className="modal">
+      <div className="modal-overlay" onClick={props.close} />
+      <div className="modal-content" onClick={null}>
+        <h1>MODAL</h1>
+      </div>
+    </div>
+  )
+}
+
 export const NavBar = props => {
+  const [displayModal, toggleModal] = useState(false);
+
   return (
     <div className="navbar">
-      <button type="button">About</button>
+      <button type="button" onClick={() => toggleModal(!displayModal)}>About</button>
       <button type="button">Guide</button>
       <button type="button">Preferences</button>
+      {displayModal && <Modal close={() => toggleModal(!displayModal)} />}
     </div>
   );
 };
