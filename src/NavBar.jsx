3b581820e2397eaf825000906552b3833e7a1bd3
holden <holdensheftel@gmail.com>
2020-11-09 20:48:36 -0700

    Update navbar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -4,6 +4,8 @@ import {about} from "./infoText/about.jsx";
 import {guide} from "./infoText/guide.jsx";
 import {latexResources} from "./infoText/latexResources.jsx";
 import {preferences} from "./infoText/preferences.jsx";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faGithub} from "@fortawesome/free-brands-svg-icons";
 
 const Modal = props => {
   return (
@@ -21,10 +23,11 @@ export const NavBar = props => {
 
   return (
     <div className="navbar">
-      <button type="button" onClick={() => toggleModal(about)}>About</button>
-      <button type="button" onClick={() => toggleModal(guide)}>Guide</button>
-      <button type="button" onClick={() => toggleModal(latexResources)}>LaTeX Resources</button>
-      <button type="button" onClick={() => toggleModal(preferences)}>Preferences</button>
+      <button className="nav-button" type="button" onClick={() => toggleModal(about)}>About</button>
+      <button className="nav-button" type="button" onClick={() => toggleModal(guide)}>Guide</button>
+      <button className="nav-button" type="button" onClick={() => toggleModal(latexResources)}>LaTeX Resources</button>
+      <button className="nav-button" type="button" onClick={() => toggleModal(preferences)}>Preferences</button>
+      <a className="nav-button github" href="https://github.com/holdenout/tree2tex" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /></a>
       {displayModal && <Modal page={displayModal} close={() => toggleModal(false)} />}
     </div>
   );
