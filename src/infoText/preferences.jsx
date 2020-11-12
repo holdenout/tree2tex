219ac0e64e636fe92a4c97b1c4749c8332761cf9
holden <holdensheftel@gmail.com>
2020-11-11 22:49:58 -0700

    Added toggle switch for tabs

--- src/infoText/preferences.jsx
+++ src/infoText/preferences.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faArrowLeft} from "@fortawesome/free-solid-svg-icons";
+import "./preferences.css";
+
+const ToggleSwitch = props => {
+  return (
+    <span className="toggle-switch-container">
+      {props.left}
+      <label className="toggle-switch">
+        <input type="checkbox" />
+        <span  className="toggle">
+          <FontAwesomeIcon className="toggle-control" icon={faArrowLeft} />
+        </span>
+      </label>
+      {props.right}
+    </span>
+  );
+}
 
 export const preferences = (
   <div>
     <h1>Preferences</h1>
     <div className="content">
+      <span className="preference-title">Indent Style: </span>
+      <ToggleSwitch left="Spaces" right="Tabs" />
       <p>In development...</p>
     </div>
   </div>
